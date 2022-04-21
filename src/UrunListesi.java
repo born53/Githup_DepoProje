c4601ac04910910f6c6dfd36c8be05a42a3e0eff
mustafa terzi <mszsv53@gmail.com>
2022-04-22 02:17:18 +0300

    Ahmet beye selamlar 22/04/2022

--- src/UrunListesi.java
+++ src/UrunListesi.java
@@ -1,3 +1,6 @@
 public class UrunListesi {
-
+    public static void main(String[] args) {
+        System.out.println("Ahmet beye selamlar");
+        System.out.println("Ahmet beye uygulamalari power pointten takip ediniz");
+    }
 }
